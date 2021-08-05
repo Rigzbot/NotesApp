24c329fdcedbc08c8110b437478c9716e7105408
Rishik Gupta <rishikgupta8@gmail.com>
2021-08-05 10:28:17 +0530

    Tutorial Build Complete excluding delete note functionality

---
+++ app/src/main/java/com/example/notesapp/CreateNoteFragment.kt
@@ -0,0 +1,424 @@
+package com.example.notesapp
+
+import android.annotation.SuppressLint
+import android.app.Activity.RESULT_OK
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.net.Uri
+import android.os.Bundle
+import android.os.PatternMatcher
+import android.provider.MediaStore
+import android.util.Log
+import android.util.Patterns
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.example.notesapp.database.NotesDatabase
+import com.example.notesapp.entities.Notes
+import com.example.notesapp.util.NoteBottomSheetFragment
+import kotlinx.android.synthetic.main.fragment_create_note.*
+import kotlinx.android.synthetic.main.fragment_create_note.tvDateTime
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.item_rv_notes.*
+import kotlinx.android.synthetic.main.item_rv_notes.view.*
+import kotlinx.coroutines.launch
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.jar.Manifest
+
+class CreateNoteFragment : BaseFragment(),
+    EasyPermissions.PermissionCallbacks, EasyPermissions.RationaleCallbacks {
+
+    var selectedColor = "#171C26"
+
+    private var READ_STORAGE_PERM = 123 //storage code
+    private var REQUEST_CODE_IMAGE = 456
+    private var selectedImagePath = ""
+    private var webLink = ""
+    private var noteId = -1
+
+    private var currentDate: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        noteId = requireArguments().getInt("noteId", -1)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_note, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            CreateNoteFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //checking if we called for single note
+        if(noteId != -1) {
+            //if yes, then rendering single note on edit note screen
+            launch {
+                context?.let {
+                    var notes = NotesDatabase.getDatabase(it).noteDao().getSpecificNote(noteId)
+
+                    colorView.setBackgroundColor(Color.parseColor(notes.color))
+
+                    etNoteDesc.setText(notes.noteText)
+                    etNoteSubTitle.setText(notes.subTitle)
+                    etNoteTitle.setText(notes.title)
+
+                    //setting image of note
+                    if (notes.imgPath != ""){
+                        selectedImagePath = notes.imgPath!!
+                        imgNote.setImageBitmap(BitmapFactory.decodeFile(notes.imgPath))
+                        layoutImage.visibility = View.VISIBLE
+                        imgNote.visibility = View.VISIBLE
+                        imgDelete.visibility = View.VISIBLE
+                    }else{
+                        layoutImage.visibility = View.GONE
+                        imgNote.visibility = View.GONE
+                        imgDelete.visibility = View.GONE
+                    }
+
+                    //setting link of note
+                    if (notes.webLink != ""){
+                        webLink = notes.webLink!!
+                        tvWebLink.text = notes.webLink
+                        layoutWebUrl.visibility = View.VISIBLE
+                        imgUrlDelete.visibility = View.VISIBLE
+                        etWebLink.setText(notes.webLink)
+                    }else{
+                        imgUrlDelete.visibility = View.GONE
+                        layoutWebUrl.visibility = View.GONE
+                    }
+                }
+            }
+        }
+
+        //register broadcast receiver
+        LocalBroadcastManager.getInstance(requireContext()).registerReceiver(
+            BroadcastReceiver, IntentFilter("bottom_sheet_action")
+        )
+
+        val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+        currentDate = sdf.format(Date())
+
+        colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+        tvDateTime.text = currentDate
+
+        imgDone.setOnClickListener {
+            if(noteId != -1) {
+                updateNote()
+            } else {
+                saveNote()
+            }
+        }
+
+        imgBack.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+
+        //open bottom sheet on button click
+        imgMore.setOnClickListener {
+            val noteBottomSheetFragment = NoteBottomSheetFragment.newInstance()
+            noteBottomSheetFragment.show(
+                requireActivity().supportFragmentManager,
+                "Note Bottom Sheet Fragment"
+            )
+        }
+
+        //on click listener for Ok button for web link
+        btnOk.setOnClickListener {
+            if (etWebLink.text.toString().trim().isNotEmpty()) {
+                checkWebUrl()
+            } else {
+                Toast.makeText(requireContext(), "Url is required", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        //on click listener for cancel button for webUrl
+        btnCancel.setOnClickListener {
+            if(noteId != -1) {
+                tvWebLink.visibility = View.VISIBLE
+                layoutWebUrl.visibility = View.GONE
+            } else {
+                layoutWebUrl.visibility = View.GONE
+            }
+        }
+
+        //click listener when clicking on link in note
+        tvWebLink.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(etWebLink.text.toString()))
+            startActivity(intent)
+        }
+
+        //click listener for delete image button
+        imgDelete.setOnClickListener {
+            selectedImagePath = ""
+            layoutImage.visibility = View.GONE
+        }
+
+        //click listener for link cancel button
+        imgUrlDelete.setOnClickListener {
+            webLink = ""
+            tvWebLink.visibility = View.GONE
+            imgUrlDelete.visibility = View.GONE
+            layoutWebUrl.visibility = View.GONE
+        }
+    }
+
+    private fun updateNote() {
+        launch {
+            context?.let {
+                val notes = NotesDatabase.getDatabase(it).noteDao().getSpecificNote(noteId)
+
+                notes.title = etNoteTitle.text.toString()
+                notes.subTitle = etNoteSubTitle.text.toString()
+                notes.noteText = etNoteDesc.text.toString()
+                notes.dateTime = currentDate
+                notes.color = selectedColor
+                notes.imgPath = selectedImagePath
+                notes.webLink = webLink
+
+                NotesDatabase.getDatabase(it).noteDao().updateNote(notes)
+                etNoteDesc.setText("")
+                etNoteSubTitle.setText("")
+                etNoteTitle.setText("")
+                layoutImage.visibility = View.GONE
+                imgNote.visibility = View.GONE
+                tvWebLink.visibility = View.GONE
+                requireActivity().supportFragmentManager.popBackStack()
+            }
+        }
+    }
+
+    private fun saveNote() {
+        //checking if entry is valid or not
+        if (etNoteTitle.text.isNullOrEmpty()) {
+            Toast.makeText(context, "Note Title is Required", Toast.LENGTH_SHORT).show()
+        } else if (etNoteSubTitle.text.isNullOrEmpty()) {
+            Toast.makeText(context, "Note Sub Title is Required", Toast.LENGTH_SHORT).show()
+        } else if (etNoteDesc.text.isNullOrEmpty()) {
+            Toast.makeText(context, "Note Description is Required", Toast.LENGTH_SHORT).show()
+        } else {
+            //coroutine to save note in database
+            launch {
+                val notes = Notes()
+                notes.title = etNoteTitle.text.toString()
+                notes.subTitle = etNoteSubTitle.text.toString()
+                notes.noteText = etNoteDesc.text.toString()
+                notes.dateTime = currentDate
+                notes.color = selectedColor
+                notes.imgPath = selectedImagePath
+                notes.webLink = webLink
+                context?.let {
+                    NotesDatabase.getDatabase(it).noteDao().insertNotes(notes)
+                    etNoteDesc.setText("")
+                    etNoteSubTitle.setText("")
+                    etNoteTitle.setText("")
+                    layoutImage.visibility = View.GONE
+                    imgNote.visibility = View.GONE
+                    tvWebLink.visibility = View.GONE
+                    requireActivity().supportFragmentManager.popBackStack()
+                }
+            }
+        }
+    }
+
+    //check if link is valid or not
+    private fun checkWebUrl() {
+        if (Patterns.WEB_URL.matcher(etWebLink.text.toString()).matches()) {
+            layoutWebUrl.visibility = View.GONE
+            etWebLink.isEnabled = false
+            webLink = etWebLink.text.toString()
+
+            tvWebLink.visibility = View.VISIBLE
+            tvWebLink.text = etWebLink.text.toString()
+        } else {
+            Toast.makeText(requireContext(), "Url is not valid", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    //create broadcast receiver to receive actions
+    private val BroadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(p0: Context?, p1: Intent?) {
+
+            var actionColor = p1!!.getStringExtra("action")
+
+            when (actionColor!!) {
+                "Blue" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+                }
+                "Yellow" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+                }
+                "Purple" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+                }
+                "Green" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+                }
+                "Orange" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+                }
+                "Black" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+                }
+                "Image" -> {
+                    readStorageTask()
+                    layoutWebUrl.visibility = View.GONE
+                }
+                "WebUrl" -> {
+                    layoutWebUrl.visibility = View.VISIBLE
+
+                }
+                else -> {
+                    layoutImage.visibility = View.GONE
+                    imgNote.visibility = View.GONE
+                    layoutWebUrl.visibility = View.VISIBLE
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        LocalBroadcastManager.getInstance(requireContext()).unregisterReceiver(BroadcastReceiver)
+        super.onDestroy()
+    }
+
+    private fun hasReadStoragePerm(): Boolean {
+        return EasyPermissions.hasPermissions(
+            requireContext(),
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+    }
+
+    private fun readStorageTask() {
+        if (hasReadStoragePerm()) {
+            pickImageFromGallery()
+        } else {
+            EasyPermissions.requestPermissions(
+                requireActivity(),
+                getString(R.string.storage_permission_text),
+                READ_STORAGE_PERM,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        }
+    }
+
+    private fun pickImageFromGallery() {
+        var intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivityForResult(intent, REQUEST_CODE_IMAGE)
+        }
+    }
+
+    @SuppressLint("Recycle")
+    private fun getPathFromUri(contentUri: Uri): String? {
+        var filePath: String? = null
+        var cursor = requireActivity().contentResolver.query(contentUri, null, null, null, null)
+        if (cursor == null) {
+            filePath = contentUri.path
+        } else {
+            cursor.moveToFirst()
+            var index = cursor.getColumnIndex("_data")
+            filePath = cursor.getString(index)
+            cursor.close()
+        }
+        return filePath
+    }
+
+    //display image on create note screen
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_IMAGE && resultCode == RESULT_OK) {
+            if (data != null) {
+                var selectedImageUrl = data.data
+                if (selectedImageUrl != null) {
+                    try {
+                        var inputStream =
+                            requireActivity().contentResolver.openInputStream(selectedImageUrl)
+                        var bitmap = BitmapFactory.decodeStream(inputStream)
+                        imgNote.setImageBitmap(bitmap)
+                        layoutImage.visibility = View.VISIBLE
+                        imgNote.visibility = View.VISIBLE
+
+                        selectedImagePath = getPathFromUri(selectedImageUrl)!!
+                    } catch (e: Exception) {
+                        Toast.makeText(requireContext(), e.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        EasyPermissions.onRequestPermissionsResult(
+            requestCode,
+            permissions,
+            grantResults,
+            requireActivity()
+        )
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(requireActivity(), perms)) {
+            AppSettingsDialog.Builder(requireActivity()).build().show()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRationaleAccepted(requestCode: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRationaleDenied(requestCode: Int) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/notesapp/HomeFragment.kt
@@ -0,0 +1,121 @@
+package com.example.notesapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.notesapp.adapter.NotesAdapter
+import com.example.notesapp.database.NotesDatabase
+import com.example.notesapp.entities.Notes
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.collections.ArrayList
+
+class HomeFragment : BaseFragment() {
+    var arrNotes = ArrayList<Notes>()
+    var notesAdapter: NotesAdapter = NotesAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //recycler view settings
+        recycler_view.setHasFixedSize(true)
+        recycler_view.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+
+        //getting all notes in launch coroutine scope
+        launch {
+            context?.let {
+                var notes = NotesDatabase.getDatabase(it).noteDao().getAllNotes()
+                notesAdapter.setData(notes)
+                arrNotes = notes as ArrayList<Notes>
+                recycler_view.adapter = notesAdapter
+            }
+        }
+
+        notesAdapter.setOnClickListener(onClicked)
+
+        //fab on click listener
+        fabBtnCreateNote.setOnClickListener {
+            replaceFragment(CreateNoteFragment.newInstance(),false)
+        }
+
+        //searching for a note
+        search_view.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onQueryTextChange(newText: String?): Boolean {
+                val tempArr = ArrayList<Notes>()
+
+                for(arr in arrNotes) {
+                    if(arr.title!!.toLowerCase(Locale.getDefault()).contains(newText.toString())) {
+                        tempArr.add(arr)
+                    }
+                }
+
+                notesAdapter.setData(tempArr)
+                notesAdapter.notifyDataSetChanged()
+                return true
+            }
+
+        })
+    }
+
+    private val onClicked = object :NotesAdapter.OnItemClickListener{
+        override fun onClicked(noteId: Int) {
+            var fragment :Fragment
+            var bundle = Bundle()
+            bundle.putInt("noteId",noteId)
+            fragment = CreateNoteFragment.newInstance()
+            fragment.arguments = bundle
+
+            replaceFragment(fragment,false)
+        }
+
+    }
+
+    //animation for fragment change
+    fun replaceFragment(fragment:Fragment, isTransition:Boolean){
+        val fragmentTransition = requireActivity().supportFragmentManager.beginTransaction()
+
+        if (isTransition){
+            fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
+        }
+        fragmentTransition.replace(R.id.frame_layout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
+    }
+
+    interface onItemClickListener {
+        fun onClicked(notesModel: Notes)
+    }
+}
